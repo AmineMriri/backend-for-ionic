3cde57d4a94a2d004bc72eac5276a387a3a0c7d0
Amine Mriri <aminemriri0@gmail.com>
2023-10-16 14:47:55 +0100

    after

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,10 @@
-from rest_framework import serializers
+from rest_framework import serializers, viewsets
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
